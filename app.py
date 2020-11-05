22364a229e44739dd8213b073e56efec7ea349e6
alishahwee <alisha.hwee@outlook.com>
2020-11-04 19:49:55 -0600

    Initialize boilerplate flask server

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+from os import getenv
+import requests
+
+# Load env vars
+load_dotenv()
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DB_URI")
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+@app.route("/")
+def homepage():
+    """Serve entry HTML file."""
+
+    render_template("index.html")
