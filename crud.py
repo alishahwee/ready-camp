d96e365199882413a2cbfd2538a0dee69512f099
alishahwee <alisha.hwee@outlook.com>
2020-11-06 13:32:41 -0600

    Scope out function stubs for CRUD

--- crud.py
+++ crud.py
@@ -1,1 +1,73 @@
 """A file for CRUD operations."""
+
+
+def get_park_coords(park_id):
+    """Get coordinates from park id."""
+
+    # TODO
+
+
+def get_park_activities(park_id):
+    """Get activities from park id."""
+
+    # TODO
+
+
+def get_park_images(park_id):
+    """Get image URLs from park id."""
+
+    # TODO
+
+
+def get_park_info(park_id):
+    """Get park info from park id."""
+
+    # TODO
+
+
+def get_park_list():
+    """Get a list of all the parks."""
+
+    # TODO
+
+
+def get_park_id(park_name):
+    """Get park id from name."""
+
+    # TODO
+
+
+def get_weather(park_id):
+    """Get weather from park id."""
+
+    # TODO
+
+
+def get_favorites(user_id):
+    """Get favorite parks from user id."""
+
+    # TODO
+
+
+def register_user(username, password, email):
+    """Register a new user."""
+
+    # TODO
+
+
+def login_user(username, password):
+    """Login a user."""
+
+    # TODO
+
+
+def get_items(is_rainy=False, is_winter=False, is_optional=False):
+    """Get all camping items."""
+
+    # TODO
+
+
+def get_user_items(user_id, park_id):
+    """Get items from user and park id."""
+
+    # TODO
