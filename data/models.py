afb501944bf92e3c4b8cc4e5308c066d84f9da5f
alishahwee <alisha.hwee@outlook.com>
2020-11-06 14:50:02 -0600

    Make emails unique

--- data/models.py
+++ data/models.py
@@ -84,7 +84,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), nullable=False, unique=True)
     password = db.Column(db.String(30), nullable=False)
-    email = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
 
     # favorite = a list of Favorite objects
     # user_item = a list of UserItem objects
