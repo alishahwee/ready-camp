3a8bdf0b4517cec988440eae77747b7ae2b3f6e1
alishahwee <alisha.hwee@outlook.com>
2020-11-10 17:04:58 -0600

    Import fonts for theme

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import { GlobalStyles } from "twin.macro";
+import "fontsource-pattaya";
+import "fontsource-poppins";
 
 const App = () => (
   <>
