4decebb584068f16d1ef755295d1334ce1bfd279
alishahwee <alisha.hwee@outlook.com>
2020-11-10 17:04:43 -0600

    Config screen sizes and fonts

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  future: {},
+  purge: [],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    fontFamily: {
+      display: ['Pattaya', 'sans-serif'],
+      body: ['Poppins', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
