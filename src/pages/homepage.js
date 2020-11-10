25354794445ff053c58fcc0abd8db0457d07718c
alishahwee <alisha.hwee@outlook.com>
2020-11-10 15:16:58 -0600

    Create test homepage

---
+++ src/pages/homepage.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Helmet from "react-helmet";
+
+const Homepage = () => (
+  <>
+    <Helmet>
+      <title>Homepage</title>
+    </Helmet>
+    <h1>Testing homepage.</h1>
+  </>
+);
+
+export default Homepage;
