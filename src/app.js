8e66135e43a7b9d8afe4ad06bd48f19d0a86682b
alishahwee <alisha.hwee@outlook.com>
2020-11-10 16:21:11 -0600

    Add global styles from Tailwind

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
+import { GlobalStyles } from "twin.macro";
 
 const App = () => (
-  <Switch>
-    <Route path="/">
-      <Homepage />
-    </Route>
-  </Switch>
+  <>
+    <GlobalStyles />
+    <Switch>
+      <Route path="/">
+        <Homepage />
+      </Route>
+    </Switch>
+  </>
 );
 
 export default App;
