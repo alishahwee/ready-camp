89f09c3301238e39e9ceb9a40e44fe254602ee27
alishahwee <alisha.hwee@outlook.com>
2020-11-10 17:04:25 -0600

    Test twin.macro prop with configs

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import Helmet from "react-helmet";
+import tw from "twin.macro";
 
 const Homepage = () => (
   <>
     <Helmet>
       <title>Homepage</title>
     </Helmet>
-    <h1>Testing homepage.</h1>
+    <h1 tw="font-body">Testing homepage.</h1>
   </>
 );
 
