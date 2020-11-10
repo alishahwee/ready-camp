23fb11b6747ca5a9ca11076953ee5d3903e44322
alishahwee <alisha.hwee@outlook.com>
2020-11-10 15:16:30 -0600

    Upgrade webpack and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,40 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-	entry: './src/index.jsx',
-	output: {
-		path: path.resolve(__dirname, 'static'),
-		filename: 'bundle.js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-				options: {
-					presets: ['@babel/preset-env', '@babel/preset-react'],
-					plugins: [
-						'babel-plugin-macros',
-						[
-							'@emotion/babel-plugin-jsx-pragmatic',
-							{
-								export: 'jsx',
-								import: '__cssprop',
-								module: '@emotion/core',
-							},
-						],
-						['babel-plugin-transform-react-jsx', { pragma: '__cssprop' }],
-					],
-				},
-			},
-		],
-	},
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "static"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: [
+              "babel-plugin-macros",
+              [
+                "@emotion/babel-plugin-jsx-pragmatic",
+                {
+                  export: "jsx",
+                  import: "__cssprop",
+                  module: "@emotion/core",
+                },
+              ],
+              [
+                "babel-plugin-transform-react-jsx",
+                {
+                  pragma: "__cssprop",
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
