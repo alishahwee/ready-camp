582ead60bad1fd4c9da004450fff570bae946498
alishahwee <alisha.hwee@outlook.com>
2020-11-11 17:47:47 -0600

    Integrate search bar

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import Helmet from "react-helmet";
 import tw from "twin.macro";
+import SearchBar from "../components/search-bar";
 
 const Homepage = () => (
   <>
     <Helmet>
       <title>Homepage</title>
     </Helmet>
-    <h1 tw="font-body">Testing homepage.</h1>
+    <SearchBar />
   </>
 );
 
