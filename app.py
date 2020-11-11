59d32f9a5437a0e67aa52cd5e6c6b8fb89d325a9
alishahwee <alisha.hwee@outlook.com>
2020-11-11 12:57:45 -0600

    Create route to serve entire parks list

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, jsonify
 from dotenv import load_dotenv
+from data.models import connect_to_db
 from crud import *
 
 # Load env vars
 load_dotenv()
 
 app = Flask(__name__)
+connect_to_db(app)
 
 
 @app.route("/")
@@ -13,3 +15,12 @@ def homepage():
     """Load the homepage."""
 
     return render_template("index.html")
+
+
+@app.route("/api/parks")
+def parks():
+    """Return all parks in JSON."""
+
+    parks = get_park_list()
+
+    return jsonify([{"id": park.id, "name": park.name} for park in parks])
