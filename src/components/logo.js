4e527f4cdb635578dd25f89bcf66ff700f5bf0be
alishahwee <alisha.hwee@outlook.com>
2020-11-11 21:16:08 -0600

    Add a logo

---
+++ src/components/logo.js
@@ -0,0 +1,8 @@
+import React from "react";
+import tw from "twin.macro";
+
+const LogoWrapper = tw.h1`font-display text-5xl text-gold-crayola`;
+
+const Logo = () => <LogoWrapper>Ready, Camp!</LogoWrapper>;
+
+export default Logo;
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -2,14 +2,16 @@ import React, { useState } from "react";
 import Helmet from "react-helmet";
 import tw from "twin.macro";
 import SearchBar from "../components/search-bar";
+import Logo from "../components/logo";
 
-const Wrapper = tw.div`flex items-center h-screen p-4`;
+const Wrapper = tw.div`flex flex-col justify-center items-center h-screen p-4 text-center`;
 
 const Homepage = () => (
   <Wrapper>
     <Helmet>
       <title>Homepage</title>
     </Helmet>
+    <Logo />
     <SearchBar />
   </Wrapper>
 );
