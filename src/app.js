ba8387d1461fded372fca5e52fa095b68fa1029e
alishahwee <alisha.hwee@outlook.com>
2020-11-12 11:28:58 -0600

    Create route path for park component

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
+import ParkPage from "./pages/park-page"
 import { GlobalStyles } from "twin.macro";
 import "fontsource-pattaya";
 import "fontsource-poppins";
@@ -9,8 +10,11 @@ const App = () => (
   <>
     <GlobalStyles />
     <Switch>
-      <Route path="/">
+      <Route exact path="/">
         <Homepage />
+      </Route>
+      <Route path="/park/:id">
+        <ParkPage />
       </Route>
     </Switch>
   </>
