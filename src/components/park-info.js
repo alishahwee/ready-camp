4ef9e42872d7b945ed0b22f675b8ee7c756c8609
alishahwee <alisha.hwee@outlook.com>
2020-11-12 16:05:39 -0600

    Create component for park info

---
+++ src/components/park-info.js
@@ -0,0 +1,22 @@
+import React from "react";
+import tw from "twin.macro";
+
+const H1 = tw.h1`font-body text-xl text-steal-teal`;
+const P = tw.p`font-body text-lg`;
+const A = tw.a`text-blue-500 hover:underline`;
+
+const ParkInfo = ({ name, address, activities, url }) => (
+  <div>
+    <H1>{name}</H1>
+    <P>{address}</P>
+    <p>Known for:</p>
+    <ul tw="list-disc">
+      {activities.map((activity) => <li tw="ml-4">{activity}</li>)}
+    </ul>
+    <A href={url} target="_blank">
+      Official Website
+    </A>
+  </div>
+);
+
+export default ParkInfo;
