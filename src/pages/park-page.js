bf7cf205cf43ff5f4a5ce2266bb989d4b9220d5a
alishahwee <alisha.hwee@outlook.com>
2020-11-12 11:57:21 -0600

    Destructure park data from JSON object

--- src/pages/park-page.js
+++ src/pages/park-page.js
@@ -1,21 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Helmet from "react-helmet";
 import tw from "twin.macro";
 
 const ParkPage = () => {
   const { id } = useParams();
+  const [data, setData] = useState({});
+  const { activities, address, coordinates, images, name, url} = data;
 
   useEffect(() => {
     fetch(`/api/park/${id}`)
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => setData(data));
   }, []);
 
   return (
     <>
       <Helmet>
-        <title></title>
+        <title>{name}</title>
       </Helmet>
       <p>Showing page for park of id: {id}</p>
     </>
