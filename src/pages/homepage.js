a9e97afce513461f532693361f4205c47d7f15dc
alishahwee <alisha.hwee@outlook.com>
2020-11-13 14:18:52 -0600

    Add bg image

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import Helmet from "react-helmet";
-import tw from "twin.macro";
+import tw, { styled } from "twin.macro";
+import bgHomepage from "../assets/bgHomepage.png";
 import SearchBar from "../components/search-bar";
 import Logo from "../components/logo";
 
-const Wrapper = tw.div`flex flex-col justify-center items-center h-screen p-4 text-center`;
+const Wrapper = styled.div`
+  ${tw`flex flex-col justify-center items-center h-screen p-4 text-center bg-cover bg-center`}
+  background-image: url(${bgHomepage});
+`;
 
 const Homepage = () => (
   <Wrapper>
