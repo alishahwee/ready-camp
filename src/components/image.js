577d8c7de4a34ee1f5bcc0fa59acca1971342f27
alishahwee <alisha.hwee@outlook.com>
2020-11-13 17:07:24 -0600

    Create responsive image

--- src/components/image.js
+++ src/components/image.js
@@ -1,8 +1,12 @@
 import React from "react";
 import tw from "twin.macro";
 
-const ImgWrapper = tw.img`rounded-xl`;
+const ImgWrapper = tw.div`rounded m-2 md:m-4 pb-1/2 md:pr-1/2 md:pb-1/3 lg:pr-2/3 relative overflow-hidden`;
 
-const Image = ({ src, alt }) => <ImgWrapper src={src} alt={alt} />;
+const Image = ({ src, alt }) => (
+  <ImgWrapper>
+    <img tw="absolute object-cover w-full h-full" src={src} alt={alt} />
+  </ImgWrapper>
+);
 
 export default Image;
