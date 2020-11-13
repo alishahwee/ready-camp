f62a51c3ff7cb5be49c7576a78f610a37031fb44
alishahwee <alisha.hwee@outlook.com>
2020-11-13 14:48:26 -0600

    Add description, styled

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -6,9 +6,11 @@ import SearchBar from "../components/search-bar";
 import Logo from "../components/logo";
 
 const Wrapper = styled.div`
-  ${tw`flex flex-col justify-center items-center h-screen p-4 text-center bg-cover bg-center`}
+  ${tw`flex flex-col justify-center items-center h-screen p-4 text-center bg-cover bg-center pb-32 sm:pb-4`}
   background-image: url(${bgHomepage});
 `;
+
+const pStyle = tw`text-white text-lg mt-2 mb-4 font-medium tracking-wide`;
 
 const Homepage = () => (
   <Wrapper>
@@ -16,6 +18,7 @@ const Homepage = () => (
       <title>Homepage</title>
     </Helmet>
     <Logo />
+    <p css={pStyle}>An all-in-one camping planner for Minnesota state parks</p>
     <SearchBar />
   </Wrapper>
 );
