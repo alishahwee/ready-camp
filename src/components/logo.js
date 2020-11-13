c9facab92d1c70879faf143251120d5badb6eb61
alishahwee <alisha.hwee@outlook.com>
2020-11-13 14:42:25 -0600

    Make font bigger

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import tw from "twin.macro";
 
-const LogoWrapper = tw.h1`font-display text-5xl text-gold-crayola`;
+const LogoWrapper = tw.h1`font-display text-7xl leading-tight text-gold-crayola`;
 
 const Logo = () => <LogoWrapper>Ready, Camp!</LogoWrapper>;
 
