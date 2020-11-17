920b33a22e0313def22da2d9d2da9c6f68b55f77
alishahwee <alisha.hwee@outlook.com>
2020-11-17 16:54:04 -0600

    Render the header on park pages

--- src/pages/park-page.js
+++ src/pages/park-page.js
@@ -3,12 +3,13 @@ import { useParams } from "react-router-dom";
 import Helmet from "react-helmet";
 import tw from "twin.macro";
 import Loading from "../components/loading";
+import Header from "../components/header";
 import Image from "../components/image";
 import ParkInfo from "../components/park-info";
 import Weather from "../components/weather";
 import Map from "../components/map";
 
-const Wrapper = tw.div`grid grid-cols-1 justify-items-center p-4`;
+const Wrapper = tw.div`grid grid-cols-1 justify-items-center pt-10 px-4`;
 const InfoCard = tw.div`flex flex-col my-3 w-full bg-lavender-fade p-2 rounded-lg max-w-screen-lg overflow-hidden shadow`;
 
 const ParkPage = () => {
@@ -37,24 +38,27 @@ const ParkPage = () => {
   }, []);
 
   return dataLoaded ? (
-    <Wrapper>
-      <Helmet>
-        <title>{name}</title>
-      </Helmet>
-      <InfoCard tw="md:flex-row">
-        <Image src={imgUrls[0]} alt={`A snapshot of ${name}`} />
-        <ParkInfo
-          name={name}
-          address={address}
-          activities={activities}
-          url={url}
-        />
-      </InfoCard>
-      <InfoCard tw="p-0">
-        <Weather parkId={id} />
-        <Map initLng={coords.lon} initLat={coords.lat} initZoom={12} />
-      </InfoCard>
-    </Wrapper>
+    <>
+      <Header />
+      <Wrapper>
+        <Helmet>
+          <title>{name}</title>
+        </Helmet>
+        <InfoCard tw="md:flex-row">
+          <Image src={imgUrls[0]} alt={`A snapshot of ${name}`} />
+          <ParkInfo
+            name={name}
+            address={address}
+            activities={activities}
+            url={url}
+          />
+        </InfoCard>
+        <InfoCard tw="p-0">
+          <Weather parkId={id} />
+          <Map initLng={coords.lon} initLat={coords.lat} initZoom={12} />
+        </InfoCard>
+      </Wrapper>
+    </>
   ) : (
     <Wrapper tw="items-center h-screen">
       <Loading />
