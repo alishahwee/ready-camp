b21ce5493ecd25fb221cde3a560c079108889e38
alishahwee <alisha.hwee@outlook.com>
2020-11-17 15:41:48 -0600

    Add responsiveness

--- src/components/weather.js
+++ src/components/weather.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import tw from "twin.macro";
 
 const Wrapper = tw.div`self-center p-4`;
-const MainView = tw.div`grid grid-flow-row grid-rows-2 grid-cols-2 gap-4 p-4 rounded-lg bg-white`;
+const MainView = tw.div`grid grid-flow-row grid-rows-2 grid-cols-2 gap-4 p-4 rounded-lg bg-white place-items-center`;
 
 const Weather = ({ parkId }) => {
   const [realtime, setRealtime] = useState(null);
@@ -36,9 +36,9 @@ const Current = ({ icon, description, temp, windSpeed }) => (
     <h1 tw="font-body text-xl text-steal-teal">Current Weather</h1>
     <MainView>
       <div tw="h-20 w-20 row-span-2 col-span-1" dangerouslySetInnerHTML={{ __html: icon }} />
-      <p tw="text-5xl row-span-2 col-span-1">{temp}</p>
+      <p tw="text-4xl md:text-5xl row-span-2 col-span-1">{temp}</p>
       <p tw="row-span-1 col-span-1">{description}</p>
-      <p tw="row-span-1 col-span-1">{windSpeed}</p>
+      <p tw="row-span-1 col-span-1">Wind: {windSpeed}</p>
     </MainView>
   </>
 );
