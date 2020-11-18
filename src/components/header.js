353cd3aa3c53714b925469550b160ea7d8663c78
alishahwee <alisha.hwee@outlook.com>
2020-11-17 21:12:56 -0600

    Render fave buttons in correct position

--- src/components/header.js
+++ src/components/header.js
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 import tw from "twin.macro";
 import Logo from "./logo";
 import SearchBar from "./search-bar";
+import { Faves } from "./btns";
 
-const Wrapper = tw.nav`flex w-screen fixed py-2 px-4 mb-2 bg-sage shadow rounded-b-md z-10`;
+const Wrapper = tw.nav`flex justify-between content-center w-screen fixed py-2 px-4 mb-2 bg-sage shadow rounded-b-md z-10`;
 
 const Header = () => (
   <Wrapper>
     <Link to="/"><Logo textSize={tw`text-2xl`} /></Link>
+    <Faves />
   </Wrapper>
 );
 
