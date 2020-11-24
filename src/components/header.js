9fef122d4b7654cd5ba7e07077d052230119ebc3
alishahwee <alisha.hwee@outlook.com>
2020-11-24 12:26:58 -0600

    Render btns depending on token

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import tw from "twin.macro";
+import { useAuth } from "../hooks/auth";
 import Logo from "./logo";
 import SearchBar from "./search-bar";
-import { Faves } from "./btns";
+import { Faves, LogoutBtn, LoginBtn } from "./btns";
 
 const Wrapper = tw.nav`flex justify-between content-center w-screen fixed py-2 px-4 mb-2 bg-sage shadow rounded-b-md z-10`;
 
-const Header = () => (
-  <Wrapper>
-    <Link to="/"><Logo textSize={tw`text-2xl`} /></Link>
-    <Faves />
-  </Wrapper>
-);
+const Header = () => {
+  const auth = useAuth();
+
+  return (
+    <Wrapper>
+      <Link to="/">
+        <Logo textSize={tw`text-3xl`} />
+      </Link>
+      {auth.token ? (
+        <div>
+          <Faves />
+          <LogoutBtn />
+        </div>
+      ) : <LoginBtn />}
+    </Wrapper>
+  );
+};
 
 export default Header;
