7da59d15d252871db959907c8a40776ebd234e83
alishahwee <alisha.hwee@outlook.com>
2020-11-24 12:26:40 -0600

    Create fave, login, logout btns

--- src/components/btns.js
+++ src/components/btns.js
@@ -1,11 +1,57 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import tw from "twin.macro";
+import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as fasHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as farHeart } from "@fortawesome/free-regular-svg-icons";
+import { useAuth } from "../hooks/auth";
+
+const RegBtn = tw.button`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded`;
 
 export const Faves = () => (
   <Link>
-    <FontAwesomeIcon icon={faHeart} size="lg" tw="text-red-400" />
+    <FontAwesomeIcon icon={fasHeart} size="lg" tw="text-red-400 mx-2" />
+  </Link>
+);
+
+export const FaveBtn = () => (
+  <button>
+    <FontAwesomeIcon icon={farHeart} size="lg" tw="text-red-400" />
+  </button>
+);
+
+export const UnfaveBtn = () => (
+  <button>
+    <FontAwesomeIcon icon={fasHeart} size="lg" tw="text-red-400" />
+  </button>
+);
+
+export const LogoutBtn = () => {
+  const auth = useAuth();
+
+  const logOut = () => {
+    auth.setToken(null);
+    axios
+      .post("/auth/logout", {
+        headers: {
+          Authorization: "Bearer " + auth.token,
+        },
+      })
+      .then((res) => {
+        console.log(res.data.message);
+      })
+      .catch(
+        (err) =>
+          err.response.status === 401 && console.log(err.response.data.message)
+      );
+  };
+
+  return <RegBtn onClick={logOut}>Log Out</RegBtn>;
+};
+
+export const LoginBtn = () => (
+  <Link to="/login">
+    <RegBtn>Log In</RegBtn>
   </Link>
 );
