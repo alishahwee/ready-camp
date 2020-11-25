2f13e29f6b95d98417269beffff99ce5e7df38f6
alishahwee <alisha.hwee@outlook.com>
2020-11-24 18:33:22 -0600

    Have checked items successfully populate db

--- src/components/items.js
+++ src/components/items.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import tw from "twin.macro";
+import { useAuth } from "../hooks/auth";
 
 const Wrapper = tw.div``;
 
-const Items = () => {
+const Items = ({ parkId }) => {
+  const auth = useAuth();
   const [categories, setCategories] = useState({});
 
   useEffect(() => {
@@ -16,6 +18,58 @@ const Items = () => {
       .catch((err) => console.log(err));
   }, []);
 
+  const updateUserItemDb = (e) => {
+    e.target.checked ? checkItem(e.target.id) : uncheckItem(e.target.id);
+  };
+
+  const checkItem = (eId) => {
+    const itemId = eId.split("-")[1]
+    axios({
+      method: "POST",
+      url: `/api/${parkId}/check`,
+      data: {
+        "item_id": itemId
+      },
+      headers: {
+        Authorization: "Bearer " + auth.token,
+      },
+    }).then((res) => {
+      if (res.data.status === "success") {
+        // TODO: Fetch and update item state.
+        console.log(res.data.message);
+      }
+    })
+    .catch(
+      (err) =>
+        err.response.status === 401 &&
+        auth.setToken(null) /* Silently log user out. */
+    );
+  };
+
+  const uncheckItem = (eId) => {
+    const itemId = eId.split("-")[1]
+    axios({
+      method: "DELETE",
+      url: `/api/${parkId}/uncheck`,
+      data: {
+        "item_id": itemId
+      },
+      headers: {
+        Authorization: "Bearer " + auth.token,
+      },
+    }).then((res) => {
+      if (res.data.status === "success") {
+        // TODO: Fetch and update item state.
+        console.log(res.data.message);
+      }
+    })
+    .catch(
+      (err) =>
+        err.response.status === 401 &&
+        auth.setToken(null) /* Silently log user out. */
+    );
+  };
+
   return Object.keys(categories).map((category) => (
     <Wrapper key={`item-wrapper-${category}`}>
       <p key={`category-${category}`}>{category}</p>
@@ -25,6 +79,7 @@ const Items = () => {
             type="checkbox"
             id={`item-${item.id}`}
             key={`checkbox-${item.id}`}
+            onChange={updateUserItemDb}
           />
           <label htmlFor={`item-${item.id}`} key={`label-${item.id}`}>
             {item.name}
