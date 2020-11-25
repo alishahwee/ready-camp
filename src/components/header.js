5700179aef4c7122af1fad1b27ed5f4704814276
alishahwee <alisha.hwee@outlook.com>
2020-11-24 21:51:44 -0600

    Delete unused import

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import tw from "twin.macro";
 import { useAuth } from "../hooks/auth";
 import Logo from "./logo";
-import SearchBar from "./search-bar";
 import { Faves, LogoutBtn, LoginBtn } from "./btns";
 
 const Wrapper = tw.nav`flex justify-between content-center w-screen fixed py-2 px-4 mb-2 bg-sage shadow rounded-b-md z-10`;
