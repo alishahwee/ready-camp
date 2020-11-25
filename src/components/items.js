217afa752cb967fee784140d5a98f4156a81395a
alishahwee <alisha.hwee@outlook.com>
2020-11-24 20:29:52 -0600

    Have items still render with no logged in user

--- src/components/items.js
+++ src/components/items.js
@@ -125,7 +125,25 @@ const Items = ({ parkId }) => {
           })}
         </Wrapper>
       ))
-    : null;
+    : auth.token ||
+        Object.keys(categories).map((category) => (
+          <Wrapper key={`item-wrapper-${category}`}>
+            <p key={`category-${category}`}>{category}</p>
+            {Object.values(categories[category]).map((item) => (
+              <div key={`item-input-group-${item.id}`}>
+                <input
+                  type="checkbox"
+                  id={`item-${item.id}`}
+                  key={`checkbox-${item.id}`}
+                  onChange={updateUserItemDb}
+                />
+                <label htmlFor={`item-${item.id}`} key={`label-${item.id}`}>
+                  {item.name}
+                </label>
+              </div>
+            ))}
+          </Wrapper>
+        ));
 };
 
 export default Items;
