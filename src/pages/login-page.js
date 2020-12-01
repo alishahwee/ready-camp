32a2494b95f89c1b51a4e2eb4891ded9646512f5
alishahwee <alisha.hwee@outlook.com>
2020-12-01 17:01:50 -0600

    Add a background image for register/login

--- src/pages/login-page.js
+++ src/pages/login-page.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Helmet from "react-helmet";
-import tw from "twin.macro";
+import tw, { styled } from "twin.macro";
 import Header from "../components/header";
 import Login from "../components/login";
+import bgRegisterLogin from "../assets/bgRegisterLogin.jpg";
 
-export const Wrapper = tw.div`flex justify-center items-center h-screen p-4 pt-16 bg-yellow-100`;
+export const Wrapper = styled.div`
+  ${tw`flex justify-center items-center h-screen bg-cover bg-center p-4 pt-16 bg-yellow-100`}
+  background-image: url(${bgRegisterLogin});
+`;
 
 const LoginPage = () => (
   <>
