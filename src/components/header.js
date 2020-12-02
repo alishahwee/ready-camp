620e78e34f9a50f7eb2585297e3b275e2bfde1e3
alishahwee <alisha.hwee@outlook.com>
2020-12-02 16:15:59 -0600

    Create a toggleable search component for navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import tw from "twin.macro";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { useAuth } from "../hooks/auth";
 import Logo from "./logo";
+import SearchBar from "./search-bar";
 import { Faves, LogoutBtn, LoginBtn } from "./btns";
 
-const Wrapper = tw.nav`flex justify-between content-center items-center w-screen fixed py-2 px-4 mb-2 bg-artichoke shadow rounded-b-md z-10 font-body`;
+const Wrapper = tw.nav`flex justify-between content-center items-center w-screen py-2 px-4 bg-artichoke shadow rounded-b-md font-body`;
 
 const Header = () => {
   const auth = useAuth();
   const location = useLocation();
+  const [showSearch, setShowSearch] = useState(false);
+
+  const toggleSearch = () => setShowSearch(!showSearch);
 
   return (
-    <Wrapper>
-      <Link to="/">
-        <Logo textSize={tw`text-2xl md:text-3xl`} />
-      </Link>
-      {auth.token ? (
-        <div>
-          <Faves />
-          <LogoutBtn />
+    <div tw="fixed z-10">
+      <Wrapper>
+        <Link to="/">
+          <Logo textSize={tw`text-2xl md:text-3xl`} />
+        </Link>
+        <div tw="flex">
+          <button onClick={toggleSearch}>
+            <FontAwesomeIcon icon={faSearch} size="md" tw="mx-2" />
+          </button>
+          {auth.token ? (
+            <div>
+              <Faves />
+              <LogoutBtn />
+            </div>
+          ) : (
+            location.pathname != "/login" && <LoginBtn />
+          )}
         </div>
-      ) : (
-        location.pathname != "/login" && <LoginBtn />
-      )}
-    </Wrapper>
+      </Wrapper>
+      {showSearch ? (
+        <div tw="absolute max-w-sm bg-lavender-fade pt-2 px-2 rounded-lg right-0 shadow">
+          <SearchBar />
+        </div>
+      ) : null}
+    </div>
   );
 };
 
